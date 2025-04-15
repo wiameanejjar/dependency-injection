c8a63dc8bba41030bf24d4fd53bd9b88134758d4
wiameanejjar <wiameanejjar@gmail.com>
2025-04-15 15:58:25 +0100

    troisième commit modifié partie de SpringAnnotation

--- src/main/java/net/anejjar/dao/DaoImpl.java
+++ src/main/java/net/anejjar/dao/DaoImpl.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 @Component("d")
-//@Repository("d")
+
 public class DaoImpl implements IDao {
     @Override
     public double getData() {
